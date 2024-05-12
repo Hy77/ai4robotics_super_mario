9b2e3407de749fd81dbfb29fe866665d335618c1
haiyang <tedsun7@gmail.com>
2024-05-12 19:31:14 +1000

    best model ver8

--- test_model.py
+++ test_model.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 import gym
 import imageio
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(1400, 900))
-display.start()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -31,14 +29,16 @@ env = make_env(skip_frames=1)
 
 action_size = env.action_space.n
 
-mario_cnn = torch.load('cnn_models/cnn_model_ver8.pth')
+version_num = 8
+
+mario_cnn = torch.load(f'cnn_models/cnn_model_ver{version_num}.pth')
 mario_cnn.eval()
 
 agent = Agent(state_size=256, action_size=action_size)
-agent.model_local = torch.load('dqn_models/dqn_model_ver8.pth')
+agent.model_local = torch.load(f'dqn_models/dqn_model_ver{version_num}.pth')
 agent.model_local.eval()
 
-num_episodes = 3
+num_episodes = 50
 scores = []
 max_x_pos = 0
 best_episode_frames = []
@@ -54,6 +54,7 @@ for i_episode in range(1, num_episodes + 1):
     while not done:
         action = agent.act(state_features.cpu().numpy(), 0.1)
         next_state, reward, done, info = env.step(action)
+        env.render()
         frame = env.render(mode='rgb_array')
         episode_frames.append(frame.copy())  # 存储当前episode的帧
         score += reward
@@ -72,9 +73,19 @@ env.close()
 
 # 保存跑得最远的那个episode的视频
 video_path = 'videos'
-writer = imageio.get_writer(f'{video_path}/best_mario_run.mp4', fps=30)
+writer = imageio.get_writer(f'{video_path}/best_mario_run_ver{version_num}.mp4', fps=30)
 for frame in best_episode_frames:
     writer.append_data(frame)
 writer.close()
 
 print(f"Best score: {max(scores)}")
+
+
+'''
+The BEST model is: ver8 -> until 12/May
+
+1st: ver8 & 7
+2nd: ver
+3rd: ver6
+
+'''
